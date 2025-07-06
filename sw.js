e460aed75f3eab0186ca1d5f458be2a9c9a8333b
ISHTIAQUE BIN IDREES <31996263+binidrees0492@users.noreply.github.com>
2025-07-06 12:40:01 +0500

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,24 @@
+const cacheName = "battery-backup-cache-v1";
+const assets = [
+  "./",
+  "./index.html",
+  "./manifest.json",
+  "./icons/icon-192.png",
+  "./icons/icon-512.png"
+];
+
+self.addEventListener("install", (e) => {
+  e.waitUntil(
+    caches.open(cacheName).then((cache) => {
+      return cache.addAll(assets);
+    })
+  );
+});
+
+self.addEventListener("fetch", (e) => {
+  e.respondWith(
+    caches.match(e.request).then((response) => {
+      return response || fetch(e.request);
+    })
+  );
+});
