eb74e0732cf9a78e3d48b3656553cb6c09e97d50
ISHTIAQUE BIN IDREES <31996263+binidrees0492@users.noreply.github.com>
2025-07-06 13:18:43 +0500

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,24 +1,6 @@
-const cacheName = "battery-backup-cache-v1";
-const assets = [
-  "./",
-  "./index.html",
-  "./manifest.json",
-  "./icons/icon-192.png",
-  "./icons/icon-512.png"
-];
-
-self.addEventListener("install", (e) => {
-  e.waitUntil(
-    caches.open(cacheName).then((cache) => {
-      return cache.addAll(assets);
-    })
-  );
+self.addEventListener('install', e => {
+  console.log('Service Worker installed');
 });
-
-self.addEventListener("fetch", (e) => {
-  e.respondWith(
-    caches.match(e.request).then((response) => {
-      return response || fetch(e.request);
-    })
-  );
+self.addEventListener('fetch', e => {
+  // Offline support could go here
 });
